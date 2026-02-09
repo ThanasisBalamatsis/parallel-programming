966b9f80469b180a3c14be516ab61a5dbf6632a7
Thanasis Balamatsis <thanasis.b@yahoo.com>
2026-02-09 19:58:56 +0200

    Add Task.WhenAny example

---
+++ ParallelProgramming/TaskWhenEach/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using TaskWhenEach;
+
+var runnerOne = new Runner(Guid.NewGuid());
+var runnerTwo = new Runner(Guid.NewGuid());
+var runnerThree = new Runner(Guid.NewGuid());
+var runnerFour = new Runner(Guid.NewGuid());
+var runnerFive = new Runner(Guid.NewGuid());
+
+using var cancellationTokenSource = 
+    new CancellationTokenSource(TimeSpan.FromSeconds(30));
+
+Console.WriteLine("Runners will start soon...");
+
+var stopwatch = Stopwatch.StartNew();
+
+List<Task<string>> simulationTasks =
+[
+    runnerOne.Run(cancellationTokenSource.Token),
+    runnerTwo.Run(cancellationTokenSource.Token),
+    runnerThree.Run(cancellationTokenSource.Token),
+    runnerFour.Run(cancellationTokenSource.Token),
+    runnerFive.Run(cancellationTokenSource.Token)
+];
+
+try
+{
+    while (simulationTasks.Count > 0)
+    {
+        var completedTask = await Task.WhenAny(simulationTasks)
+            .WaitAsync(cancellationTokenSource.Token);
+
+        simulationTasks.Remove(completedTask);
+
+        var message = await completedTask;
+        Console.WriteLine(message);
+    }
+
+    Console.WriteLine($"Simulation ended. Total simulation duration {stopwatch.ElapsedMilliseconds}ms");
+}
+catch (TaskCanceledException)
+{
+    Console.WriteLine("ERROR: Runners didn't finish on time...");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: An unexpected error occurred: {ex.Message}");
+}
---
+++ ParallelProgramming/TaskWhenEach/Runner.cs
@@ -0,0 +1,24 @@
+﻿namespace TaskWhenEach;
+
+internal class Runner
+{
+    public Guid Id { get; }
+    public TimeSpan SimulationDuration { get; }
+
+    public Runner(Guid id)
+    {
+        Id = id;
+        SimulationDuration = TimeSpan.FromSeconds(Random.Shared.Next(1,7));
+    }
+
+    public async Task<string> Run(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Simulation for runner with id {Id} starts...");
+
+        await Task.Delay(SimulationDuration, cancellationToken);
+
+        Console.WriteLine($"Simulation for runner with id {Id} ended...");
+
+        return $"Runner {Id} finished in {SimulationDuration.TotalSeconds} seconds.";
+    }
+}
