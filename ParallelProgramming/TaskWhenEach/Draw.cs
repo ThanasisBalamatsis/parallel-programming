1f5987570a2dd2471e776b527e25e434dc5b333a
Thanasis Balamatsis <thanasis.b@yahoo.com>
2026-02-10 10:39:29 +0200

    Add TaskWhenEach project

---
+++ ParallelProgramming/TaskWhenEach/Draw.cs
@@ -0,0 +1,27 @@
+﻿namespace TaskWhenEach;
+
+internal class Draw
+{
+    private int _luckyNumber;
+
+    public Guid Id { get; }
+    public TimeSpan SimulationDuration { get; }
+
+    public Draw(TimeSpan simulationDuration)
+    {
+        Id = Guid.NewGuid();
+        SimulationDuration = simulationDuration;
+    }
+
+    public async Task<int> SimulateDraw(CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Simulation for draw with Id {Id} starts in thread {Thread.CurrentThread.ManagedThreadId}");
+
+        await Task.Delay(SimulationDuration, cancellationToken);
+        _luckyNumber = new Random().Next(1, 100);
+
+        Console.WriteLine($"Simulation for draw with Id {Id} ended in thread {Thread.CurrentThread.ManagedThreadId}. Lucky number is {_luckyNumber}");
+
+        return _luckyNumber;
+    }
+}
---
+++ ParallelProgramming/TaskWhenEach/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using TaskWhenEach;
+
+Console.WriteLine("Draw process will start soon");
+
+var drawOne = new Draw(TimeSpan.FromSeconds(1));
+var drawTwo = new Draw(TimeSpan.FromSeconds(4));
+var drawThree = new Draw(TimeSpan.FromSeconds(5));
+var drawFour = new Draw(TimeSpan.FromSeconds(3));
+
+using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+
+var stopwath = Stopwatch.StartNew();
+
+List<Task<int>> drawTasks =
+[
+    drawOne.SimulateDraw(cancellationTokenSource.Token),
+    drawTwo.SimulateDraw(cancellationTokenSource.Token),
+    drawThree.SimulateDraw(cancellationTokenSource.Token),
+    drawFour.SimulateDraw(cancellationTokenSource.Token)
+];
+
+try
+{
+    await foreach (Task<int> drawTask in 
+        Task.WhenEach(drawTasks)
+        .WithCancellation(cancellationTokenSource.Token))
+    {
+        var luckyNumber = await drawTask;
+
+        Console.WriteLine($"Lucky number is {luckyNumber}");
+    }
+
+    stopwath.Stop();
+
+    Console.WriteLine($"Draw process ended. Total duration {stopwath.ElapsedMilliseconds}ms");
+}
+catch (TaskCanceledException)
+{
+    Console.WriteLine("ERROR: Draws didn't finish on time...");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: An unexpected error occurred: {ex.Message}");
+}
+
